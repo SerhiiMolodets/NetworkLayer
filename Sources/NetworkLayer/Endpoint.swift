9a3a794ad4a1ccb997bdaf16dc86d863d4946773
MolodetsSerhii <s.molodets@devhive.fun>
2024-09-24 12:15:58 +0300

    add network layer

---
+++ Sources/NetworkLayer/Endpoint.swift
@@ -0,0 +1,30 @@
+//  Created by Serhii Molodets on 24.09.2024.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var method: RequestMethod { get }
+    var header: [String: String]? { get }
+    var body: [String: String]? { get }
+    var queryItems: [URLQueryItem]? { get }
+    var token: String? { get }
+    var apiKey: String? { get }
+}
+
+extension Endpoint {
+    var scheme: String {
+        return "https"
+    }
+    
+    var token: String? {
+        return nil
+    }
+    
+    var apiKey: String? {
+        return nil
+    }
+}
---
+++ Sources/NetworkLayer/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Serhii Molodets on 24.09.2024.
+//
+
+import Foundation
+
+enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
